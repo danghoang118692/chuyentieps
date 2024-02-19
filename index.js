7d33cae37b94d03368cfa1be146fd590715278df
danghoang118692 <160466622+danghoang118692@users.noreply.github.com>
2024-02-19 15:19:23 +0700

    index.js

--- index.js
+++ index.js
@@ -1,23 +1,12 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
+const express = require('express');
+const app = express();
 
-async function handleRequest(request) {
-  // Thay thế 'https://example.com' bằng URL của trang web bạn muốn chuyển hướng đến
-  const redirectTo = 'https://page.facebook-kr.com/';
+// Chuyển hướng đến https://google.com
+app.get('/', (req, res) => {
+    res.redirect('https://google.com');
+});
 
-  // Lấy tham số query từ URL hiện tại (?ref=)
-  const currentURL = new URL(request.url);
-  const queryRef = currentURL.searchParams.get('ref');
-
-  // Tạo URL mới với tham số query (?www.facebook.com/help/1380418588640631)
-  const redirectURL = new URL(redirectTo);
-  if (queryRef) {
-    redirectURL.searchParams.set('next', 'www.facebook.com');
-    redirectURL.searchParams.set('help', '1380418588640631');
-    redirectURL.searchParams.set('id', queryRef);
-  }
-
-  // Thực hiện chuyển hướng với URL mới
-  return Response.redirect(redirectURL, 301);
-}
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
